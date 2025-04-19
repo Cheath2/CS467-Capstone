31f447ad15d10da36f3fe234c528927c4c898e8a
Cheath2 <heathca@oregonstate.edu>
2025-04-19 15:11:21 -0400

    Initial Backend Scaffolding for server environment

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => res.send('API running'));
+
+// Connect to MongoDB and start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error('DB connection error:', err));
