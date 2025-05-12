465bda113617892aa33d989b50541b5a021c6121
Rutu Patel <122194363+patel-hub@users.noreply.github.com>
2025-05-12 17:44:40 +0000

    fixed routing issues

--- Skills/routes/skillRoutes.js
+++ Skills/routes/skillRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Skill = require('../models/Skill');
+const Skill = require('../models/Skills');
 const verifyToken = require('../middleware/verifyToken');
 
 // @route   POST /api/yourskills
